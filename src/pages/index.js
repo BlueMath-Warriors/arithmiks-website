3aae804ba93c582657efc9858f40812223ea4497
NivyanB <bnivyan09@gmail.com>
2023-09-21 15:17:16 +0500

    added engagement model to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,5 +15,4 @@ export const Head = () => (<>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"></link>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"></link>
-<link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
 </>);
