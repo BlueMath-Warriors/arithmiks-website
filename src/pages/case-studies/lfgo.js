bb1ea26f3bc47146f81bee22412205cb0f7779d8
iabdullah928 <iabdullah928@gmail.com>
2026-01-30 19:50:06 +0500

    Created case study detail page of LFGO

---
+++ src/pages/case-studies/lfgo.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Lfgo from "../../components/CaseStudy/Lfgo";
+import { SEO } from "../../components/seo";
+
+const LfgoPage = () => {
+  return <Lfgo />;
+};
+
+export default LfgoPage;
+
+export const Head = () => (
+  <SEO
+    title={"LFGO - Arithmiks"}
+  />
+);
+
